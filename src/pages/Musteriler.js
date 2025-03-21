a4d7099a70f37b81992f2fd407d0259dc0761105
onurozturkler <onurozturkler@gmail.com>
2025-03-21 16:35:29 +0300

    Update Musteriler.js

--- src/pages/Musteriler.js
+++ src/pages/Musteriler.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { db } from "../firebase";
 import { collection, getDocs, deleteDoc, doc, addDoc } from "firebase/firestore";
+import "../index.css";
 import * as XLSX from "xlsx";
 
 const Musteriler = () => {
