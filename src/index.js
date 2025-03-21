48629e1bd802e941998e147604a4d2764af3fb14
onurozturkler <onurozturkler@gmail.com>
2025-03-21 10:59:17 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client"; // Burayı değiştirdik!
 import App from "./App";
+import "./index.css";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
