7febd2f2008d9fd403401b05c4c99f7ebdab1a94
onurozturkler <onurozturkler@gmail.com>
2025-03-21 04:05:20 +0300

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ function Navbar() {
           <img 
             src="https://esca-food.com/image/catalog/esca-food-logo.png" 
             alt="EscaFood Logo" 
-            className="h-6 w-auto transform scale-35"
+            className="h-[259px] w-[333px] md:h-[259px] md:w-[333px]"
           />
         </Link>
 
